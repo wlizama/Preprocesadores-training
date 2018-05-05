e02b2a8470f36b56340a7b47afbab42c4fdb962a
Wilder Lizama <wilderlizama@gmail.com>
2018-05-04 20:51:43 -0500

    init Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var stylus = require('gulp-stylus');
 
+
+// SASS tasks
 gulp.task('sass', function () {
   return gulp.src('./Sass/src/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
@@ -11,4 +14,16 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./Sass/src/*.scss', ['sass']);
+});
+
+
+// Stylus tasks
+gulp.task('stylus', function () {
+  return gulp.src('./Stylus/src/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./Stylus/dist'));
+});
+
+gulp.task('stylus:watch', function () {
+  gulp.watch('./Stylus/src/*.styl', ['stylus']);
 });
